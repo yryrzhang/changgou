c193c7d554ca45dde7279bc408df366b722e37ca
ZHANGGO3 <gordon.zhang@oocl.com>
2020-04-23 16:43:21 +0800

    init project

---
+++ changgou-parent/changgou_eureka/src/main/java/EurekaApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : Gordon Zhang
+ * @Date: 4/23/2020 3:29 PM
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
